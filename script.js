69e64b15063aef141b13186f6ec7e93238833f6c
Trinity-prog <Jamison.t.025@gmail.com>
2026-08-07 21:56:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ const careerDetails = detailsButton.nextElementSibling;
 function showCareerMessage() {
 	alert("Thank you for visisting my career goals website!");
 }
+function validateContactForm() {
+	var name = document.getElementByID("name").value.trim();
+	var email = document.getElementByID("email").value.trim();
+	var reason = document.getElementByID("reason").value.trim();
+	var message = document.getElementByID("message").value.trim();
+	var form-message = document.getElementByID("form-message").value.trim();
+	if (name === "" || email === "" || reason === "" || message === "") {
+	formMessage.textContent = "Please complete all fields before submitting.";
+	formMessage.style.color = "Red";
+	return false;
+}
+if (!email.includes("@")) {
+		formMessage.textContent = "Please enter a valid email address.";
+		formMessage.style.color = "Red";
+		return false;
+}
+formMessage.textContent = "Thank You! Your form was completed successfully.";
+formMessage.style.color = "Green";
+document.getElementByID("contact-form").reset();
+return false;
+}
+
+
 
